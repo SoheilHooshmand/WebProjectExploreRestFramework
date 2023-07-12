b3190391d5d1bbb6ecb12c382515027b36d8148e
SoheilHooshmand <99813498+SoheilHooshmand@users.noreply.github.com>
2023-07-12 10:48:06 +0330

    add serializer.py

---
+++ profiles/serializer.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Profile, Skill, Message
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'email', 'username', 'password']
+
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    #user = UserSerializer(many = False)
+    skills = serializers.SerializerMethodField()
+    #messages = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+    def get_skills(self, obj):
+        skills = obj.skill_set.all()
+        serializer = SkillSerializer(skills, many = True)
+        return serializer.data
+
+    # def get_messages(self, obj):
+    #     messages = obj.messages.all()
+    #     serializer = MessageSerializer(messages, many = True)
+    #     return serializer.data
+
