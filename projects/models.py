b07e7e99a99c00a7a6627421fd9d7363171fba5e
SoheilHooshmand <99813498+SoheilHooshmand@users.noreply.github.com>
2023-07-20 11:00:05 +0330

    add models.py

---
+++ projects/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from profiles.models import Profile
+import uuid
+
+class Project(models.Model):
+    name = models.CharField(max_length = 200)
+    owner = models.ForeignKey(Profile, null = True, blank = True, on_delete = models.SET_NULL)
+    image = models.ImageField(null = True, blank = True)
+    description = models.TextField(null = True, blank = True)
+    demo_link = models.CharField(max_length = 2000, null = True, blank = True)
+    source_link = models.CharField(max_length = 200, null = True, blank = True)
+    tags = models.ManyToManyField('Tag', null = True, blank = True)
+    vote_total = models.IntegerField(default = 0, null = True, blank = True)
+    vote_ratio = models.IntegerField(default = 0, null = True, blank = True)
+    created = models.DateTimeField(auto_now_add = True)
+    id = models.UUIDField(default = uuid.uuid4, unique = True, primary_key = True, editable = True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-vote_ratio', '-vote_total', 'name']
+
+
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list('owner__id', flat = True)
+        return queryset
+
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value = 'up').count()
+        totalVotes = reviews.count()
+        ratio = (upVotes / totalVotes) * 100
+        self.vote_total = totalVotes
+        self.vote_ratio = ratio
+        self.save()
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'up vote'),
+        ('down', 'down vote')
+    )
+    owner = models.ForeignKey(Profile, on_delete = models.SET_NULL, null = True)
+    project = models.ForeignKey(Project, on_delete = models.CASCADE)
+    body = models.TextField(null = True, blank = True)
+    value = models.CharField(max_length = 200, choices = VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add = True)
+    id = models.UUIDField(default = uuid.uuid4, unique = True, primary_key = True, editable = False)
+
+    class Meta:
+       unique_together = [['owner', 'project']]
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length = 200)
+    created = models.DateTimeField(auto_now_add = True)
+    id = models.UUIDField(default = uuid.uuid4, unique = True, primary_key = True, editable = False)
+
+    def __str__(self):
+        return self.name
+
+
