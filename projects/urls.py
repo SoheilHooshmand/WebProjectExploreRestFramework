439fc1a4bad75c954ee8cdb4c844cad4c8648ffb
SoheilHooshmand <99813498+SoheilHooshmand@users.noreply.github.com>
2023-07-23 11:54:50 +0330

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProfilesList.as_view()),
+    path('', views.ProjectList.as_view()),
     path('project/<str:pk>/', views.PorjectDetails.as_view()),
     path('review/<str:pk>/', views.ReviewList.as_view()),
     path('tag/', views.TagList.as_view()),
